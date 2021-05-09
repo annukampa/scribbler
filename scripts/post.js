065786787a236b64caa82786be39c0ac48cf9e44
adixit <adixit@wdtablesystems.com>
2021-05-10 01:04:33 +0530

    Changes for editable heading and content.

--- scripts/post.js
+++ scripts/post.js
@@ -1,11 +1,12 @@
 let commentsArr = [];
+let postData;
 function openSignUpBox() {
     $('#signInBox').modal('hide');
     $('#signUpBox').modal('show');
 }
 
 function renderEditPostDiv() {
-    const postData = JSON.parse(localStorage.getItem('item-to-edit'));
+    postData = JSON.parse(localStorage.getItem('item-to-edit'));
     let headerEl = document.getElementById('edit-header');
     let subHeaderEl = document.getElementById('edit-sub-header-el');
     let contentDiv = document.getElementById('post-content');
@@ -19,4 +20,32 @@ function editPost() {
     document.getElementById('edit-header').className = 'editable-box-border';
     document.getElementById('post-content').setAttribute("contenteditable", "true");
     document.getElementById('post-content').className = 'editable-box-border';
+    let subDiv = document.getElementsByClassName('edit-sub-header')[0];
+    let editButton = document.getElementById('edit-button');
+    editButton.style.display = 'none';
+    subDiv.innerHTML = '<span id="edit-sub-header-el">' + postData.writer  +'</span>\n' +
+        '<button id="save-button" class="btn post-page-btn" onclick="savePost()">Save\n' +
+        '                    <i class="fa fa-floppy-o icon-left-align" aria-hidden="true"></i>\n' +
+        '                </button>';
+
+}
+
+function savePost() {
+    console.log('in save post @#@#@#@');
+    document.getElementById('edit-header').removeAttribute("contenteditable");
+    document.getElementById('edit-header').classList.remove('editable-box-border');
+    document.getElementById('edit-header').style.textAlign = 'center';
+    postData.heading = 'UPDATED : ' + postData.heading;
+    document.getElementById('edit-header').innerText = postData.heading;
+    let contentDiv = document.getElementById('post-content');
+    contentDiv.innerHTML = '<div>UPDATED : </div>\n' + postData.content;
+    document.getElementById('post-content').removeAttribute("contenteditable");
+    document.getElementById('post-content').classList.remove('editable-box-border');
+    let saveBtn = document.getElementById('save-button');
+    saveBtn.remove();
+    let subDiv = document.getElementsByClassName('edit-sub-header')[0];
+    subDiv.innerHTML = '<span id="edit-sub-header-el">' + postData.writer  +'</span>\n' +
+        '                <button id="edit-button" class="btn post-page-btn" onclick="editPost()">Edit\n' +
+        '                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>\n' +
+        '                </button>'
 }
